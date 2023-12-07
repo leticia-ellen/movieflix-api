fea41a5cbba9993d4662fd5166a3a439a540d01e
Letícia Ellen <leticiaellen97@hotmail.com>
2023-12-07 11:22:37 -0300

    Adicionado swagger com cadastro de filmes

--- src/server.tsx
+++ src/server.tsx
@@ -1,11 +1,14 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
 
 const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use("/docs", swaggerUi.serve , swaggerUi.setup(swaggerDocument));
 
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
@@ -60,12 +63,58 @@ app.put("/movies/:id", async (req, res) => {
         }
 
         const data = { ...req.body };
-        
-        data.release_date = data.release_date ? new Date(data.release_date): undefined;
+
+        data.release_date = data.release_date ? new Date(data.release_date) : undefined;
 
         await prisma.movie.update({ where: { id }, data });
     } catch (error) {
         return res.status(500).send({ message: "Falha ao atualizar o registro" });
+    }
+
+});
+
+app.delete("/movies/:id", async (req, res) => {
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id }
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        await prisma.movie.delete({ where: { id } });
+
+    } catch (error) {
+        res.status(500).send({ message: "Falha ao remover o registro" });
+    }
+    res.status(200).send();
+});
+
+app.get("/movies/:genderName", async (req, res) => {
+
+    const genderName = req.params.genderName;
+
+    try {
+        const moviesFilteredByGenderName = await prisma.movie.findMany({
+            include: {
+                genres: true,
+                languages: true,
+            },
+            where: {
+                genres: {
+                    name: {
+                        equals: genderName,
+                        mode: "insensitive",
+                    },
+                },
+            },
+        });
+        res.status(200).send(moviesFilteredByGenderName);
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar um filme" });
     }
 
 });
