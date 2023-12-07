dd12cd80d4b5b8898e2f99f3cfc6dbadd6477280
Letícia Ellen <leticiaellen97@hotmail.com>
2023-12-07 09:10:47 -0300

    adiionada atualização

--- src/server.tsx
+++ src/server.tsx
@@ -20,6 +20,17 @@ app.post("/movies", async (req, res) => {
     const { title, genre_id, language_id, oscar_count, release_date } = req.body;
 
     try {
+
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: { equals: title, mode: "insensitive" }
+            },
+        });
+
+        if (movieWithSameTitle) {
+            return res.status(409).send({ message: "Já existe um filme com esse título" });
+        }
+
         await prisma.movie.create({
             data: {
                 title: title,
@@ -30,9 +41,33 @@ app.post("/movies", async (req, res) => {
             },
         });
     } catch (error) {
-        return res.status(500).send({ message: "Falha ao cadastrar um filme" })
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
     }
     res.status(201).send();
+});
+
+app.put("/movies/:id", async (req, res) => {
+
+    const id = Number(req.params.id);
+
+    try {
+        const movie = await prisma.movie.findUnique({
+            where: { id },
+        });
+
+        if (!movie) {
+            return res.status(404).send({ message: "Filme não encontrado" });
+        }
+
+        const data = { ...req.body };
+        
+        data.release_date = data.release_date ? new Date(data.release_date): undefined;
+
+        await prisma.movie.update({ where: { id }, data });
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao atualizar o registro" });
+    }
+
 });
 
 app.listen(port, () => {
