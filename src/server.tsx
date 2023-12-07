fd0990c1efd8660bd9c1140ab8a6dca60ac56529
Letícia Ellen <leticiaellen97@hotmail.com>
2023-12-07 08:32:13 -0300

    adicionado mensagem de erro na criação do filme

--- src/server.tsx
+++ src/server.tsx
@@ -1,12 +1,40 @@
-import express from "express"
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
-app.get("/movies",(req,res)=>{
-    res.send('Lista de filmes')
-})
+app.use(express.json());
 
-app.listen(port, ()=>{
+app.get("/movies", async (_, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: { title: "asc" },
+        include: { genres: true, languages: true },
+    });
+    res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+        await prisma.movie.create({
+            data: {
+                title: title,
+                genre_id: genre_id,
+                language_id: language_id,
+                oscar_count: oscar_count,
+                release_date: new Date(release_date),
+            },
+        });
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" })
+    }
+    res.status(201).send();
+});
+
+app.listen(port, () => {
     console.log(`Servidor em execução na porta ${port}`);
-})
+});
